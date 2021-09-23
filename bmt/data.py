5f84cb3d2d6d48658c3419c718aa4beb5e90bbca
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-09-23 10:35:39 -0400

    Add support for mixins and aliases

--- bmt/data.py
+++ bmt/data.py
@@ -14,12 +14,24 @@ with pkg_resources.open_text(_data, "all_slots.json") as stream:
     all_slots: List[str] = json.load(stream)
 with pkg_resources.open_text(_data, "all_types.json") as stream:
     all_types: List[str] = json.load(stream)
-with pkg_resources.open_text(_data, "ancestors.json") as stream:
-    ancestors: Dict[str, List[str]] = json.load(stream)
-with pkg_resources.open_text(_data, "descendants.json") as stream:
-    descendants: Dict[str, List[str]] = json.load(stream)
-with pkg_resources.open_text(_data, "children.json") as stream:
-    children: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "alias_ancestors.json") as stream:
+    alias_ancestors: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "basic_ancestors.json") as stream:
+    basic_ancestors: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "mixin_ancestors.json") as stream:
+    mixin_ancestors: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "alias_descendants.json") as stream:
+    alias_descendants: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "basic_descendants.json") as stream:
+    basic_descendants: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "mixin_descendants.json") as stream:
+    mixin_descendants: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "alias_children.json") as stream:
+    alias_children: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "basic_children.json") as stream:
+    basic_children: Dict[str, List[str]] = json.load(stream)
+with pkg_resources.open_text(_data, "mixin_children.json") as stream:
+    mixin_children: Dict[str, List[str]] = json.load(stream)
 with pkg_resources.open_text(_data, "parent.json") as stream:
     parent: Dict[str, Optional[str]] = json.load(stream)
 with pkg_resources.open_text(_data, "element.json") as stream:
